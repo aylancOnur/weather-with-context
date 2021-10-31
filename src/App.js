aa1dc090bdbbc3fc972c8c47da89030356fd7581
Fr3shBrain <aylanc.onur@gmail.com>
2021-10-31 23:26:55 +0300

    weather-app-with-react-context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Dropdown from "./components/Dropdown";
+import WeatherContainer from "./components/WeatherContainer";
+import { CityNameProvider } from "./context/CityContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CityNameProvider>
+        <Dropdown />
+        <WeatherContainer />
+      </CityNameProvider>
     </div>
   );
 }
